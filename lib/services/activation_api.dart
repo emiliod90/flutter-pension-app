fa140d92ded72416bbdb199a7f97f9feb1a66ae0
emiliod90 <emiliod90@gmail.com>
2020-08-26 09:29:16 +0100

    Corrected URI Endpoints for login/reg api
    Signed-off-by: emiliod90 <emiliod90@gmail.com>

--- lib/services/activation_api.dart
+++ lib/services/activation_api.dart
@@ -2,14 +2,13 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 Future ActivateUser(String email, String password, String mobile) async {
-  String endpoint = 'http://52.213.247.254:8080/appregistration/v1/gettokeninfo';
+  String endpoint = 'http://52.213.247.254:8080/activation/v1/memberactivation';
   final response = await http.put(endpoint,
       headers: {"Accept": "Application/json", "Content-Type": "application/json"},
-      body: jsonEncode({'email_id': email, 'password': password, 'mobile': mobile,'tnc': 1}));
+      body: jsonEncode({'email_id': email, 'password': password, 'mob_num': mobile,'is_tnc': 1}));
   if (response.statusCode != null) {
     var jsonResponse = jsonDecode(response.body);
     return jsonResponse;
-
   }
   else return null;
 }
--- lib/services/registration_api.dart
+++ lib/services/registration_api.dart
@@ -2,7 +2,7 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 Future RegisterUser(String id, String dob, String ni) async {
-  String endpoint = 'http://52.213.247.254:8080/appregistration/v1/gettokeninfo';
+  String endpoint = 'http://52.213.247.254:8080/appregistration/v1/registermember';
   final response = await http.put(endpoint,
       headers: {"Accept": "Application/json", "Content-Type": "application/json"},
       body: jsonEncode({'mem_id': id, 'dob': dob, 'ni_num': ni}));
